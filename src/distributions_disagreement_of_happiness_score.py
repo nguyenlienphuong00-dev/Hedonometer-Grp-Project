5316302190ef7bb8d3c5901d286f2c712b2c691c
nguyenkhanhquynh190506-lgtm <nguyenkhanhquynh190506@gmail.com>
2026-03-16 21:04:07 +0100

    Update distributions_disagreement_of_happiness_score.py

--- src/distributions_disagreement_of_happiness_score.py
+++ src/distributions_disagreement_of_happiness_score.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt # Importing the matplotlib library for plotting and setting the nickname to save typing.
+import matplotlib.pyplot as plt 
 
 df = pd.read_csv("data/raw/Data_Set_S1.txt", sep = '\t', skiprows = 2, na_values = '--')
 
@@ -8,11 +8,11 @@ df = pd.read_csv("data/raw/Data_Set_S1.txt", sep = '\t', skiprows = 2, na_values
 #2.1
 
 # Do the math (summary statistics).
-mean_val = df['happiness_average'].mean() #calcukating the mean of the happiness scores.
-median_val = df['happiness_average'].median() #calculating the median of the happiness scores.
-std_val = df['happiness_average'].std() #calculating the standard deviation of the happiness scores.
-perc_5th = df['happiness_average'].quantile(0.05) #calculating the 5th percentile of the happiness scores.
-perc_95th = df['happiness_average'].quantile(0.95) #calculating the 95th percentile of the happiness scores.
+mean_val = df['happiness_average'].mean() 
+median_val = df['happiness_average'].median() 
+std_val = df['happiness_average'].std() 
+perc_5th = df['happiness_average'].quantile(0.05) 
+perc_95th = df['happiness_average'].quantile(0.95) 
 print(f"Mean happiness score: {mean_val:.2f}")
 print(f"Median happiness score: {median_val:.2f}")
 print(f"Standard deviation of happiness score: {std_val:.2f}")
@@ -20,29 +20,29 @@ print(f"5th percentile of happiness score: {perc_5th:.2f}")
 print(f"95th percentile of happiness score: {perc_95th:.2f}")
 
 # Histogram of happiness scores.
-plt.figure(figsize=(10, 6)) #create a blank canvas that is 10 inches wide and 6 inches tall for better visualization.
-plt.hist(df['happiness_average'], bins = 50, color='skyblue', edgecolor='black') #bins=50,Python divide the range of happiness scores into 50 equal intervals, color makes the bars blue, and edgecolor puts a black outline around them so they are easy to see.
+plt.figure(figsize=(10, 6)) 
+plt.hist(df['happiness_average'], bins = 50, color='skyblue', edgecolor='black') 
 
-plt.title('Distribution of Happiness Scores') #title of the historgram.
-plt.xlabel('Happiness Score (1 to 9)') #label for x-axis.
-plt.ylabel('Frequency (Number of words)') #label for y-axis.
-plt.axvline(mean_val, color='red', linestyle='dashed', linewidth=2, label=f"mean: {mean_val:.2f}") #add a vertical dashed red line at the mean value of happiness scores, with a label showing the mean value.
-plt.axvline(median_val, color='green', linestyle='dashed', linewidth=2, label=f"median: {median_val:.2f}") #add a vertical dashed green line at the median value of happiness scores, with a label showing the median value.
-plt.legend() #creates a little key in the corner so people know what the red and green lines mean.
+plt.title('Distribution of Happiness Scores') 
+plt.xlabel('Happiness Score (1 to 9)') 
+plt.ylabel('Frequency (Number of words)')
+plt.axvline(mean_val, color='red', linestyle='dashed', linewidth=2, label=f"mean: {mean_val:.2f}") 
+plt.axvline(median_val, color='green', linestyle='dashed', linewidth=2, label=f"median: {median_val:.2f}") 
+plt.legend() 
 plt.savefig("figures/happiness_histogram.png")
-plt.show() #display the histogram.
+plt.show() 
 
 #2.2
 # Disagreement and Scatterplot.
-top_contested = df.sort_values(by='happiness_standard_deviation', ascending=False).head(15) #sort the dataset by the standard deviation of happiness scores in descending order and select the top 15 words with the highest disagreement.
+top_contested = df.sort_values(by='happiness_standard_deviation', ascending=False).head(15) 
 print(top_contested[['word', 'happiness_average', 'happiness_standard_deviation']])
 
-plt.figure(figsize =(10,6)) #create a blank canvas that is 6 inches wide and 10 inches tall for better visualization.
-plt.scatter(df['happiness_average'],df['happiness_standard_deviation'], color='green', alpha=0.4, s=15) #create a scatter plot with happiness average on the x-axis and standard deviation on the y-axis, using purple dots that are slightly transparent (alpha=0.4) and small in size (s=15).
-plt.title('Words Happiness vs Disagreement') #title of the scatter plot.
-plt.xlabel('Average Happiness Score (1 to 9)') #label for x-axis.
-plt.ylabel('Standard Deviation of Happiness Score (Disagreement)') #label for y-axis.
-plt.grid(True, linestyle= '--', alpha=0.5) #add a grid to the scatter plot with dashed lines and light transparency for better readability.
+plt.figure(figsize =(10,6)) 
+plt.scatter(df['happiness_average'],df['happiness_standard_deviation'], color='green', alpha=0.4, s=15) 
+plt.title('Words Happiness vs Disagreement') 
+plt.xlabel('Average Happiness Score (1 to 9)') 
+plt.ylabel('Standard Deviation of Happiness Score (Disagreement)') 
+plt.grid(True, linestyle= '--', alpha=0.5) 
 plt.savefig("figures/happiness_scatter.png")
-plt.show() #display the scatter plot.
+plt.show()
 
